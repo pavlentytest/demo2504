85127f29b27d82d26577e42f99b293c7bd2becae
pavlentytest <impetus@yandex.ru>
2019-04-25 14:15:02 +0300

    First commit

---
+++ App/App/AuthForm.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace App
+{
+    public partial class AuthForm : Form
+    {
+
+        Form reg;
+
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            reg = new RegForm();
+            reg.Show();
+
+        }
+    }
+}
